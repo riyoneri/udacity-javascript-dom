13ad484268ded15dd3846e446701c560ef23aa56
riyoneri <lionkaneza@gmail.com>
2023-06-21 09:40:46 +0200

    Use of DOMContentLoaded event

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const myCustomArticle = document.createElement("article")
-
-myCustomArticle.innerHTML = `
-<p>Brownie lollipop <span>carrot cake</span> gummies lemon drops sweet roll dessert tiramisu. Pudding muffin <span>cotton candy</span> croissant fruitcake tootsie roll. Jelly jujubes brownie. Marshmallow jujubes topping sugar plum jelly jujubes chocolate.</p>
-
-    <p>Tart bonbon soufflé gummi bears. Donut marshmallow <span>gingerbread cupcake</span> macaroon jujubes muffin. Soufflé candy caramels tootsie roll powder sweet roll brownie <span>apple pie</span> gummies. Fruitcake danish chocolate tootsie roll macaroon.</p>
-`
-
-myCustomArticle.addEventListener('click', e => {
-    if (e.target.nodeName === "SPAN") console.log("Span clicked " + e.target.textContent)
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const myCustomArticle = document.createElement("article")
+    
+    myCustomArticle.innerHTML = `
+    <p>Brownie lollipop <span>carrot cake</span> gummies lemon drops sweet roll dessert tiramisu. Pudding muffin <span>cotton candy</span> croissant fruitcake tootsie roll. Jelly jujubes brownie. Marshmallow jujubes topping sugar plum jelly jujubes chocolate.</p>
+    
+        <p>Tart bonbon soufflé gummi bears. Donut marshmallow <span>gingerbread cupcake</span> macaroon jujubes muffin. Soufflé candy caramels tootsie roll powder sweet roll brownie <span>apple pie</span> gummies. Fruitcake danish chocolate tootsie roll macaroon.</p>
+    `
+    
+    myCustomArticle.addEventListener('click', e => {
+        if (e.target.nodeName === "SPAN") console.log("Span clicked " + e.target.textContent)
+    })
+    
+    document.body.appendChild(myCustomArticle)
 })
-
-document.body.appendChild(myCustomArticle)
