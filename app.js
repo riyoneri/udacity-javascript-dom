3b6cd5f0e595f7ceec791d62bdf7e943c9b852d7
riyoneri <lionkaneza@gmail.com>
2023-06-21 09:18:36 +0200

    -Enhance performance on multiple events -Access event.target on parent of clicked child

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-document.addEventListener("click", event => {
-    console.log(event)
-})
+const myCustomDiv = document.createElement("div")
+
+const respondToTheClick = evt => {
+    console.log(evt.target)
+    console.log("A paragraph was clicked")
+}
+
+for (let i = 1; i <= 200; i++) {
+    const newElement = document.createElement('p');
+    newElement.textContent = 'This is paragraph number ' + i;
+
+    myCustomDiv.appendChild(newElement);
+}
+
+myCustomDiv.addEventListener("click", respondToTheClick)
+
+document.body.appendChild(myCustomDiv);
